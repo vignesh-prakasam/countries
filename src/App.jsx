bbc2ab4cb7e2bfc66bb7b3d4f491b485540a3a36
Vignesh Prakasam <vigneshp.ceg@gmail.com>
2024-10-22 14:25:52 +0530

    added icons

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   const [countries, setCountries] = useState([data]);
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [showRegion, setShowRegion] = useState(false);
-  const [currentRegion, setCurrentRegion] = useState('Filter by Region');
+  const [currentRegion, setCurrentRegion] = useState("Filter by Region");
 
   const filterCountry = (e) => {
     const filter = e.target.value;
-    setCurrentRegion('Filter by Region');
+    setCurrentRegion("Filter by Region");
     const filteredCountries = data.filter((country) =>
       country.name.toLowerCase().startsWith(filter.toLowerCase())
     );
@@ -18,55 +18,161 @@ function App() {
   };
 
   const filterRegion = () => {
-    const filter = currentRegion == 'Filter by Region' ? '' : (currentRegion == 'America' ? 'Americas' : currentRegion);
+    const filter =
+      currentRegion == "Filter by Region"
+        ? ""
+        : currentRegion == "America"
+        ? "Americas"
+        : currentRegion;
     const filteredCountries = data.filter(
-      (country) => ((country.region === filter) || filter === '') 
+      (country) => country.region === filter || filter === ""
     );
     setCountries([filteredCountries]);
   };
 
   useEffect(() => {
     filterRegion();
-  },[currentRegion])
+  }, [currentRegion]);
 
   return (
     <>
-      <header className={`flex flex-row justify-between items-center border-b-[1px] shadow-black ${isDarkMode ? 'bg-very-dark-blue text-white' : 'bg-white text-very-dark-blue'} px-4 py-8`}>
-        <h1 className={`${isDarkMode ? 'text-white' : 'text-very-dark-blue-text'} text-2xl font-bold ml-5`}>
+      <header
+        className={`drop-shadow-md  flex flex-row justify-between items-center  ${
+          isDarkMode
+            ? "bg-very-dark-blue text-white"
+            : "bg-white text-very-dark-blue"
+        } px-4 py-8`}
+      >
+        <h1
+          className={`${
+            isDarkMode ? "text-white" : "text-very-dark-blue-text"
+          } text-2xl font-bold ml-5`}
+        >
           Where in the world?
         </h1>
-        <button onClick={() => {setIsDarkMode(!isDarkMode)}} className={`text-md ${isDarkMode ? 'text-white' : 'text-very-dark-blue-text'} font-semibold mr-5`}> {isDarkMode ? "Light Mode" : "Dark Mode"}</button>
+        <button
+          onClick={() => {
+            setIsDarkMode(!isDarkMode);
+          }}
+          className={`text-md ${
+            isDarkMode ? "text-white" : "text-very-dark-blue-text"
+          } font-semibold mr-5`}
+        >
+          {isDarkMode ? (
+            <i class="fa-solid fa-moon mr-1 px-1"></i>
+          ) : (
+            <i class="fa-regular fa-moon mr-1 px-1"></i>
+          )}
+          Dark Mode
+        </button>
       </header>
-      <main className={`${isDarkMode ? 'bg-very-dark-blue-bg': 'bg-very-light-gray'}  w-full  min-h-screen px-10`}>
+      <main
+        className={`${
+          isDarkMode ? "bg-very-dark-blue-bg" : "bg-very-light-gray"
+        }  w-full  min-h-screen px-10`}
+      >
         {/* filters */}
         <div className="flex sm:flex-row flex-col sm:justify-between justify-start sm:mb-0">
-          <input
-            type="text"
-            name="country"
-            id="country"
-            className={`shadow-lg mt-10 h-14 sm:w-96 w-full rounded-md pl-10 ${isDarkMode ? 'bg-very-dark-blue text-white' : 'text-very-dark-blue-text'}`}
-            placeholder="Search for a country"
-            onKeyDown={filterCountry}
-          />
-          <div onClick={() => setShowRegion(!showRegion)}  className={`relative ${isDarkMode ? 'bg-very-dark-blue' : 'bg-white' } rounded-md p-5 w-60 h-14 my-10 flex items-center justify-between cursor-pointer`}>
-            <h1 className={`${isDarkMode ? 'text-white' : 'text-very-dark-blue-text'} `}>{currentRegion}</h1>
-            {
-              isDarkMode ?  <img src={iconArrow} className="size-5" alt="arrow" style={{ filter: 'invert(1)' }} />
-              : <img src={iconArrow} className="size-5" alt="arrow"  />
-            }
-            
-            
-            <div className={`${showRegion ? '' : 'hidden'} absolute top-5 left-0 ${isDarkMode ? 'text-white bg-very-dark-blue' : 'bg-white text-very-dark-blue-text'}  rounded-lg my-10  w-60`}>
+          <div className="relative">
+          <i class=" absolute top-12 left-4 fa-solid fa-magnifying-glass text-gray-400"></i>
+            <input
+              type="text"
+              name="country"
+              id="country"
+              className={`shadow-lg mt-7 h-14 sm:w-96 w-full rounded-md pl-10 ${
+                isDarkMode
+                  ? "bg-very-dark-blue text-white"
+                  : "text-very-dark-blue-text"
+              }`}
+              placeholder="Search for a country..."
+              onKeyDown={filterCountry}
+            />
+          </div>
+          <div
+            onClick={() => setShowRegion(!showRegion)}
+            className={`relative ${
+              isDarkMode ? "bg-very-dark-blue" : "bg-white"
+            } rounded-md p-5 w-60 h-14 my-7 flex items-center justify-between cursor-pointer`}
+          >
+            <h1
+              className={`${
+                isDarkMode ? "text-white" : "text-very-dark-blue-text"
+              } `}
+            >
+              {currentRegion}
+            </h1>
+            {isDarkMode ? (
+              <img
+                src={iconArrow}
+                className="size-5"
+                alt="arrow"
+                style={{ filter: "invert(1)" }}
+              />
+            ) : (
+              <img src={iconArrow} className="size-5" alt="arrow" />
+            )}
+
+            <div
+              className={`${showRegion ? "" : "hidden"} absolute top-5 left-0 ${
+                isDarkMode
+                  ? "text-white bg-very-dark-blue"
+                  : "bg-white text-very-dark-blue-text"
+              }  rounded-lg my-10  w-60`}
+            >
               <ul className="py-2 px-2 cursor-pointer ">
-                <li className={`py-2 pl-2 ${isDarkMode ? 'hover:bg-very-dark-blue-bg' : 'hover:bg-very-light-gray'}`} onClick={() => setCurrentRegion("Africa")}>Africa</li>
-                <li className={`py-2 pl-2 ${isDarkMode ? 'hover:bg-very-dark-blue-bg' : 'hover:bg-very-light-gray'}`} onClick={() => setCurrentRegion("America")}>America</li>
-                <li className={`py-2 pl-2 ${isDarkMode ? 'hover:bg-very-dark-blue-bg' : 'hover:bg-very-light-gray'}`} onClick={() => setCurrentRegion("Asia")}>Asia</li>
-                <li className={`py-2 pl-2 ${isDarkMode ? 'hover:bg-very-dark-blue-bg' : 'hover:bg-very-light-gray'}`} onClick={() => setCurrentRegion("Europe")}>Europe</li>
-                <li className={`py-2 pl-2 ${isDarkMode ? 'hover:bg-very-dark-blue-bg' : 'hover:bg-very-light-gray'}`} onClick={() => setCurrentRegion("Oceania")}>Oceania</li>
+                <li
+                  className={`py-2 pl-2 ${
+                    isDarkMode
+                      ? "hover:bg-very-dark-blue-bg"
+                      : "hover:bg-very-light-gray"
+                  }`}
+                  onClick={() => setCurrentRegion("Africa")}
+                >
+                  Africa
+                </li>
+                <li
+                  className={`py-2 pl-2 ${
+                    isDarkMode
+                      ? "hover:bg-very-dark-blue-bg"
+                      : "hover:bg-very-light-gray"
+                  }`}
+                  onClick={() => setCurrentRegion("America")}
+                >
+                  America
+                </li>
+                <li
+                  className={`py-2 pl-2 ${
+                    isDarkMode
+                      ? "hover:bg-very-dark-blue-bg"
+                      : "hover:bg-very-light-gray"
+                  }`}
+                  onClick={() => setCurrentRegion("Asia")}
+                >
+                  Asia
+                </li>
+                <li
+                  className={`py-2 pl-2 ${
+                    isDarkMode
+                      ? "hover:bg-very-dark-blue-bg"
+                      : "hover:bg-very-light-gray"
+                  }`}
+                  onClick={() => setCurrentRegion("Europe")}
+                >
+                  Europe
+                </li>
+                <li
+                  className={`py-2 pl-2 ${
+                    isDarkMode
+                      ? "hover:bg-very-dark-blue-bg"
+                      : "hover:bg-very-light-gray"
+                  }`}
+                  onClick={() => setCurrentRegion("Oceania")}
+                >
+                  Oceania
+                </li>
               </ul>
             </div>
           </div>
-
         </div>
         {/* countries */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-20">
@@ -74,7 +180,9 @@ function App() {
             return (
               <div
                 key={index}
-                className={`${isDarkMode ? 'bg-very-dark-blue' : 'bg-white'} rounded-md shadow-md mb-5`}
+                className={`${
+                  isDarkMode ? "bg-very-dark-blue" : "bg-white"
+                } rounded-md shadow-md mb-5`}
               >
                 <img
                   src={country.flag}
@@ -82,18 +190,34 @@ function App() {
                   className="w-full h-56 object-cover rounded-t-md"
                 />
                 <div className="p-6">
-                  <h1 className={`${!isDarkMode ? 'text-very-dark-blue-text' : 'text-white'} font-bold text-lg mb-5`}>
+                  <h1
+                    className={`${
+                      !isDarkMode ? "text-very-dark-blue-text" : "text-white"
+                    } font-bold text-lg mb-5`}
+                  >
                     {country.name}
                   </h1>
-                  <p className={`${isDarkMode ? 'text-white' : 'text-very-dark-blue-text'}`}>
+                  <p
+                    className={`${
+                      isDarkMode ? "text-white" : "text-very-dark-blue-text"
+                    }`}
+                  >
                     <span className="font-semibold">Population:</span>{" "}
                     {country.population}
                   </p>
-                  <p className={`${isDarkMode ? 'text-white' : 'text-very-dark-blue-text'}`}>
+                  <p
+                    className={`${
+                      isDarkMode ? "text-white" : "text-very-dark-blue-text"
+                    }`}
+                  >
                     <span className="font-semibold">Region:</span>{" "}
                     {country.region}
                   </p>
-                  <p className={`${isDarkMode ? 'text-white' : 'text-very-dark-blue-text'}`}>
+                  <p
+                    className={`${
+                      isDarkMode ? "text-white" : "text-very-dark-blue-text"
+                    }`}
+                  >
                     <span className="font-semibold">Capital:</span>{" "}
                     {country.capital}
                   </p>
